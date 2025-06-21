5b8e4bb9412233c45a4fdd17f00f8e635d054a44
JyothiRaja <jyothiraja14@gmail.com>
2025-06-21 23:15:40 +0530

    Add files via upload

---
+++ week1/Firefox.java
@@ -0,0 +1,16 @@
+package week1;
+
+public class Firefox {
+	public static void main(String[] args) {
+	float browserVersion = 100.2f;
+	String browserName="firefox";
+	boolean invisible=true;
+	int releaseyear=1998;
+	char browserlogo='f';
+	System.out.println("Broswer  name =" +browserName);
+	System.out.println("Browser Version =" +browserVersion);
+	System.out.println("invisible="+invisible);
+	System.out.println("Release year="+releaseyear);
+	System.out.println("Browser Logo=" +browserlogo);
+}
+}
