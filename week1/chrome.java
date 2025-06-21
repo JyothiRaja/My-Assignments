182418c61666811ea6e79fd25cea36aee4128adc
JyothiRaja <jyothiraja14@gmail.com>
2025-06-21 23:24:36 +0530

    Add files via upload

---
+++ week1/chrome.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class chrome {
+	public static void main(String[] args) {
+		float browserVersion = 91.0f;
+		String developer="Google";
+		boolean isBeta=false;
+		int releaseyear=2008;
+		char shortcutkey='f';
+		
+		System.out.println("Browser Version =" +browserVersion);
+		System.out.println("Developer =" +developer);
+		System.out.println("IsBeta="+isBeta);
+		System.out.println("Release year="+releaseyear);
+		System.out.println("Shortcutkey=" +shortcutkey);
+	
+}
+}
